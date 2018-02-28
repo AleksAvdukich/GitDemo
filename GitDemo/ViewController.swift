d6168a68438f9539f34add43b0db897bb268f09a
AleksAvdukich <34261780+AleksAvdukich@users.noreply.github.com>
2018-02-28 15:31:04 +0300

    Update ViewController with one additional print

--- GitDemo/ViewController.swift
+++ GitDemo/ViewController.swift
@@ -14,14 +14,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         print("Another print statement")
+        print("Hello Github")
         
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
